ffedb8bbf8f0fa67fc5f61b5cb4c050f0f6394a8
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-03-22 20:17:40 +0530

    Created the Model package

---
+++ src/main/java/com/nikhil/JobPortal/Model/Post.java
@@ -0,0 +1,65 @@
+package com.nikhil.JobPortal.Model;
+
+import java.util.Arrays;
+
+public class Post {
+    private String id;
+    private String desc;
+    private int exp;
+    private String profile;
+    private String[] tech;
+
+    public Post() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String[] getTech() {
+        return tech;
+    }
+
+    public void setTech(String[] tech) {
+        this.tech = tech;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id='" + id + '\'' +
+                ", desc='" + desc + '\'' +
+                ", exp=" + exp +
+                ", profile='" + profile + '\'' +
+                ", tech=" + Arrays.toString(tech) +
+                '}';
+    }
+}
