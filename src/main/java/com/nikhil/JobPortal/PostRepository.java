7f09afa80fa0e2b4552fc5515f6ab97adafc424d
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-03-22 20:19:04 +0530

    Created the DAO layer for Database

---
+++ src/main/java/com/nikhil/JobPortal/PostRepository.java
@@ -0,0 +1,7 @@
+package com.nikhil.JobPortal;
+
+import com.nikhil.JobPortal.Model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+}
