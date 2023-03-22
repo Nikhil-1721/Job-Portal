682669aed89678781fd6aec67568575a582f8fb5
Nikhil Yadav <119732887+Nikhil-1721@users.noreply.github.com>
2023-03-22 20:08:40 +0530

    Delete src directory

--- src/main/java/com/nikhil/JobPortal/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nikhil.JobPortal;
-
-import com.nikhil.JobPortal.Model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-
-}
