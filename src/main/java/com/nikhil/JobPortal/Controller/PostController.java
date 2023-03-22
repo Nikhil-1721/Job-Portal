5abd6168bd7b47de988300ad6e8ddbf7fe9a9456
Nikhil Yadav <yadav.nikhil.in@gmail.com>
2023-03-22 20:20:55 +0530

    Created method to get all the posts

--- src/main/java/com/nikhil/JobPortal/Controller/PostController.java
+++ src/main/java/com/nikhil/JobPortal/Controller/PostController.java
@@ -1,8 +1,21 @@
 package com.nikhil.JobPortal.Controller;
 
+import com.nikhil.JobPortal.Model.Post;
+import com.nikhil.JobPortal.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PostController {
-    
+
+    @Autowired
+    PostRepository repository;
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+        return repository.findAll();
+    }
 }
